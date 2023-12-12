dc00b0aae67a8b2932410f975fd64591f3e6991a
usuario <hcatalan@elpuig.xeill.net>
2023-12-12 16:59:43 +0100

    Pa que no se pierda

--- src/main/java/org/openqa/selenium/Main.java
+++ src/main/java/org/openqa/selenium/Main.java
@@ -1,17 +1,21 @@
 package org.openqa.selenium;
-
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.io.XMLWriter;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
-  public static void main(String[] args) throws InterruptedException {
+  public static void main(String[] args) throws InterruptedException, IOException {
     System.out.println(System.getenv("PATH"));
     System.out.println(System.getenv("HOME"));
 
@@ -24,37 +28,29 @@ public class Main {
     driver.get("https://www.flashscore.es/futbol/espana/laliga-ea-sports-2021-2022/#/MPV5cuep/table/overall");
 
     WebElement tournamentTable = driver.findElement(By.id("tournament-table"));
-    //wait.until(ExpectedConditions.elementToBeClickable(tournamentTable));
 
     wait.until(ExpectedConditions.elementToBeClickable(By.className("ui-table__row")));
     List<WebElement> tableRows = tournamentTable.findElements(By.className("ui-table__row"));
 
-    WebElement goleadores = driver.findElement(By.id("tournament-table"));
-    //wait.until(ExpectedConditions.elementToBeClickable(tournamentTable));
-
-
-    ArrayList<ArrayList<String>> lines = new ArrayList<ArrayList<String>>();
+    Document document = DocumentHelper.createDocument();
+    Element root = document.addElement("data");
 
     for (WebElement row : tableRows) {
-
       List<WebElement> cells = row.findElements(By.className("table__cell"));
-      ArrayList<String> line = new ArrayList<String>();
+      Element rowElement = root.addElement("row");
 
       int i = 0;
       for (WebElement cell : cells) {
-        //System.out.println(i+" "+cell.getText());
-        if (i != 9) line.add(cell.getText());
+        if (i != 9) {
+          Element cellElement = rowElement.addElement("cell");
+          cellElement.setText(cell.getText());
+        }
         i++;
-
       }
-      lines.add(line);
-
-      for (String e : line) {
-        System.out.println(e);
-      }
-      CSV csv = new CSV();
-      csv.generarCsvEstat(line);
     }
 
+    // También puedes guardar el documento XML en un archivo si es necesario
+    XMLWriter writer = new XMLWriter(new FileWriter("./archivo.xml"));
+    writer.write(document);
   }
 }
